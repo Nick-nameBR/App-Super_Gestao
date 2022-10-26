28a602878b8e43a3800df4b7d6dcbfd59259afe9
nikolas <nikolasmvsantos@gmail.com>
2022-10-26 11:32:07 -0300

    Captura e envio de dados para BD

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        var_dump($_POST);
+        //Capturando informações do formulário atraves do método request
+
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        //Enviando as informações para o banco de dados
+        $contato->save();
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
