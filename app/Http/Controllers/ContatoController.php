0c2cecd4f96039989db0e7636ce985e3cc2d93f9
nikolas <nikolasmvsantos@gmail.com>
2022-10-26 11:44:10 -0300

    Update ContatoController.php

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,7 +9,7 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        //Capturando informações do formulário atraves do método request
+        /*Capturando informações do formulário atraves do método request
 
         $contato = new SiteContato();
         $contato->nome = $request->input('nome');
@@ -18,8 +18,21 @@ class ContatoController extends Controller
         $contato->motivo_contato = $request->input('motivo_contato');
         $contato->mensagem = $request->input('mensagem');
 
-        //Enviando as informações para o banco de dados
-        $contato->save();
+        Enviando as informações para o banco de dados
+
+        $contato->save();*/
+
+        //Tornando o código acima mais simples utilizando o método fill (pois no model possuimos o método fillable)
+        $contato = new SiteContato();
+        
+        //Validação para evitar a chamada do request sempre que acessar a página
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+            $contato->fill($request->all());
+            $contato->save();
+        }
+        
+        
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
