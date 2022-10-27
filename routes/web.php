9aa9f10610dfcbd865e7f8e8e844674b3d957443
nikolas <nikolasmvsantos@gmail.com>
2022-10-27 16:38:52 -0300

    Implementando middleware nas rotas

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::Middleware(LogAcessoMiddleware::class)
-    ->get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])
-    ->name('site.index');
-    
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])
+    ->name('site.index')
+    ->Middleware(LogAcessoMiddleware::class);
+
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])
+    ->name('site.sobrenos')
+    ->Middleware(LogAcessoMiddleware::class);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])
+    ->name('site.contato')
+    ->Middleware(LogAcessoMiddleware::class);
+
 Route::post('/contato', [\App\Http\Controllers\ContatoController::class,'salvar'])->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
